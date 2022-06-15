31fb72e13cc7ba852d6bc10265a704e0ba61e3b2
Judah Gabriel Himango <judahgabriel@gmail.com>
2022-06-14 23:35:26 -0500

    Online status is computed just once

--- Web/src/script/services/online-detector.ts
+++ Web/src/script/services/online-detector.ts
@@ -5,16 +5,23 @@ import { ApiServiceBase } from "./api-service-base";
  * navigator.onLine doesn't work consistently across browser and doesn't always return true status.
  */
 export class OnlineDetector extends ApiServiceBase {
+
+    private static onlineStatus: boolean | null = null;
+
     constructor() {
         super();
     }
 
     /**
-     * Detects whether we're online by pinging the API.
+     * Detects whether we're online by pinging the API. Result of this function is memoized internally; once online status has been checked, we always return that status.
      * @returns A promise containing boolean online status.
      */
     public checkOnline(): Promise<boolean> {
-        return this.pingApiWithTimeout(3000);
+        if (OnlineDetector.onlineStatus !== null) {
+            return Promise.resolve(OnlineDetector.onlineStatus);
+        }
+        return this.pingApiWithTimeout(3000)
+            .then(onlineStatus => OnlineDetector.onlineStatus = onlineStatus);
     }
 
     private pingApiWithTimeout(timeoutMs: number): Promise<boolean> {
