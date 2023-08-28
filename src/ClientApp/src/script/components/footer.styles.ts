671350708346f06b1dc2563364673082f6b65712
Judah Gabriel Himango <judahgabriel@gmail.com>
2023-08-27 17:57:34 -0700

    Footer improvements

--- src/ClientApp/src/script/components/footer.styles.ts
+++ src/ClientApp/src/script/components/footer.styles.ts
@@ -3,6 +3,11 @@ import { css } from "lit";
 export const footerStyles = css`
     :host {
         font-family: var(--subtitle-font);
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        background-color: var(--sl-color-neutral-100);
+        width: 100%;
     }
 
     a {
--- src/ClientApp/src/script/components/footer.ts
+++ src/ClientApp/src/script/components/footer.ts
@@ -1,27 +1,21 @@
-import { LitElement, html } from 'lit';
-import { customElement } from 'lit/decorators.js';
-import { footerStyles } from './footer.styles';
-import { sharedStyles } from '../common/shared.styles';
-import { bootstrapUtilities } from '../common/bootstrap-utilities.styles';
+import { LitElement, html } from "lit";
+import { customElement } from "lit/decorators.js";
+import { footerStyles } from "./footer.styles";
+import { sharedStyles } from "../common/shared.styles";
+import { bootstrapUtilities } from "../common/bootstrap-utilities.styles";
 
-@customElement('app-footer')
+@customElement("app-footer")
 export class AppFooter extends LitElement {
     static styles = [sharedStyles, bootstrapUtilities, footerStyles];
 
     render() {
         return html`
-            <footer class="w-100 pb-5 mt-3 mb-2 d-print-none">
-                <div class="text-center">
+            <footer class="d-flex justify-content-center w-100 gap-5 p-2 d-print-none">
                     <a href="/">Home</a>
-                    <span class="bar-separator">|</span>
                     <a href="/about">About us, legal</a>
-                    <span class="bar-separator">|</span>
-                    <a href="https://blog.judahgabriel.com/2012/01/introducing-messianicchordscom.html">What is this site?</a>
-                    <span class="bar-separator">|</span>
-                    <a href="https://blog.judahgabriel.com">Author's Blog</a>
-                    <span class="bar-separator">|</span>
-                    <a href="https://messianicradio.com">Chavah Messianic Radio</a>
-                </div>
+                    <a target="_blank" href="https://blog.judahgabriel.com/2012/01/introducing-messianicchordscom.html">What is this site?</a>
+                    <a target="_blank" href="https://blog.judahgabriel.com">Author's blog</a>
+                    <a target="_blank" href="https://messianicradio.com">Chavah Messianic Radio</a>
             </footer>
         `;
     }
