3f76113bd535c8aab65a051d641f2f7d960d892b
Judah Gabriel Himango <judahgabriel@gmail.com>
2023-08-27 17:57:49 -0700

    Formatting fix

--- src/ClientApp/src/script/components/footer.ts
+++ src/ClientApp/src/script/components/footer.ts
@@ -11,11 +11,11 @@ export class AppFooter extends LitElement {
     render() {
         return html`
             <footer class="d-flex justify-content-center w-100 gap-5 p-2 d-print-none">
-                    <a href="/">Home</a>
-                    <a href="/about">About us, legal</a>
-                    <a target="_blank" href="https://blog.judahgabriel.com/2012/01/introducing-messianicchordscom.html">What is this site?</a>
-                    <a target="_blank" href="https://blog.judahgabriel.com">Author's blog</a>
-                    <a target="_blank" href="https://messianicradio.com">Chavah Messianic Radio</a>
+                <a href="/">Home</a>
+                <a href="/about">About us, legal</a>
+                <a target="_blank" href="https://blog.judahgabriel.com/2012/01/introducing-messianicchordscom.html">What is this site?</a>
+                <a target="_blank" href="https://blog.judahgabriel.com">Author's blog</a>
+                <a target="_blank" href="https://messianicradio.com">Chavah Messianic Radio</a>
             </footer>
         `;
     }
