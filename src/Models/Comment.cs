9b5dd8fb47eb39cae735d6223548602c9f9dcdb5
Judah Gabriel Himango <judahgabriel@gmail.com>
2024-04-05 11:44:14 -0700

    Adds comment threads to ChordSheets.

---
+++ src/Models/Comment.cs
@@ -0,0 +1,28 @@
+﻿namespace MessianicChords.Models
+{
+    /// <summary>
+    /// A single comment on a chord sheet.
+    /// </summary>
+    public class Comment
+    {
+        /// <summary>
+        /// Gets the ID of the user that created the comment.
+        /// </summary>
+        required public string UserId { get; set; }
+
+        /// <summary>
+        /// Gets the display name of the user who created the comment.
+        /// </summary>
+        required public string UserDisplayName { get; set; }
+
+        /// <summary>
+        /// Gets the content of the comment.
+        /// </summary>
+        required public string Content { get; set; }
+
+        /// <summary>
+        /// Gets the date when the comment was created.
+        /// </summary>
+        public DateTimeOffset Created { get; set; }
+    }
+}
---
+++ src/Models/CommentThread.cs
@@ -0,0 +1,20 @@
+﻿using Google.Apis.Drive.v3.Data;
+
+namespace MessianicChords.Models
+{
+    /// <summary>
+    /// A thread of comments attached to a chord sheet.
+    /// </summary>
+    public class CommentThread
+    {
+        /// <summary>
+        /// The ID of the ChordSheet this comment thread is attached to.
+        /// </summary>
+        required public string ChordSheetId { get; set; }
+
+        /// <summary>
+        /// The list of comments in this thread.
+        /// </summary>
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
