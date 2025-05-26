c92fad0b32180430f7c677c83ca826e73ccbf40c
Judah Gabriel Himango <judahgabriel@gmail.com>
2025-05-26 14:10:01 -0700

    Stores emails in db

---
+++ src/Models/Email.cs
@@ -0,0 +1,57 @@
+﻿using SendGrid.Helpers.Mail;
+using System.Net.Mail;
+
+namespace MessianicChords.Models;
+
+/// <summary>
+/// An outgoing email. Stored in the database to be sent at a later time.
+/// </summary>
+public class Email
+{
+    public static Email FromSmtpMessage(SendGridMessage smtpEmail)
+    {
+        return new Email
+        {
+            Body = smtpEmail.HtmlContent,
+            Bcc = string.Join("; ", smtpEmail.Personalizations.SelectMany(p => p.Bccs.Select(cc => cc.Email))),
+            CC = string.Join("; ", smtpEmail.Personalizations.SelectMany(p => p.Ccs.Select(cc => cc.Email))),
+            ReplyTo = smtpEmail.From?.ToString(),
+            Created = DateTimeOffset.UtcNow,
+            Subject = smtpEmail.Subject,
+            To = string.Join("; ", smtpEmail.Personalizations.SelectMany(p => p.Tos.Select(cc => cc.Email))),
+        };
+    }
+
+    public string? Id { get; set; }
+    public string To { get; set; } = string.Empty;
+    public string? ReplyTo { get; set; }
+    public string? CC { get; set; }
+    public string? Bcc { get; set; }
+    public string Subject { get; set; } = string.Empty;
+    public string Body { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The date the email was successfully sent.
+    /// </summary>
+    public DateTimeOffset? Sent { get; set; }
+
+    /// <summary>
+    /// When the email was created.
+    /// </summary>
+    public DateTimeOffset Created { get; set; } = DateTimeOffset.UtcNow;
+
+    /// <summary>
+    /// The last error message that occurred when sending the email.
+    /// </summary>
+    public string? SendingErrorMessage { get; set; }
+
+    /// <summary>
+    /// The date the email was attempted to be sent again after a previous failure to send.
+    /// </summary>
+    public DateTimeOffset? LastRetryDate { get; set; }
+
+    /// <summary>
+    /// The number of times the email has been retried.
+    /// </summary>
+    public int RetryCount { get; set; }
+}
--- src/Services/EmailService.cs
+++ src/Services/EmailService.cs
@@ -1,101 +1,119 @@
 ﻿using MessianicChords.Api.Models;
+using MessianicChords.Common;
 using MessianicChords.Models;
-using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Newtonsoft.Json.Linq;
-using Raven.Client.Documents.Operations.OngoingTasks;
+using Raven.Client.Documents;
 using SendGrid.Helpers.Mail;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using static System.Net.WebRequestMethods;
 
-namespace MessianicChords.Services
+namespace MessianicChords.Services;
+
+public class EmailService
 {
-    public class EmailService
+    private readonly AppSettings settings;
+    private readonly IWebHostEnvironment host;
+    private readonly ILogger<EmailService> logger;
+    private readonly IDocumentStore db;
+
+    public EmailService(IOptions<AppSettings> options, IWebHostEnvironment host, IDocumentStore db, ILogger<EmailService> logger)
     {
-        private readonly AppSettings settings;
-        private readonly IWebHostEnvironment host;
-        private readonly ILogger<EmailService> logger;
+        this.settings = options.Value;
+        this.host = host;
+        this.db = db;
+        this.logger = logger;
+    }
 
-        public EmailService(IOptions<AppSettings> options, IWebHostEnvironment host, ILogger<EmailService> logger)
-        {
-            this.settings = options.Value;
-            this.host = host;
-            this.logger = logger;
-        }
+    /// <summary>
+    /// Sends an email notifying that a chord sheet has been edited or created and is awaiting approval.
+    /// </summary>
+    /// <param name="submission"></param>
+    /// <param name="original">The original chord sheet being edited. If this is a new chord sheet being submitted, this will be null.</param>
+    /// <param name="token">The approval token.</param>
+    /// <returns></returns>
+    public async Task SendChordSubmissionEmail(ChordSubmission submission, ChordSheet? original, string token)
+    {
+        var htmlBody = original == null ?
+            this.GetNewChordsEmailHtml(submission, token) :
+            this.GetUpdatedChordsEmailHtml(submission, token);
+        var email = this.CreateEmail("MessianicChords: chord chart edit awaiting your approval", new EmailAddress(settings.UploadedAttachmentEmailRecipient), null, htmlBody);
+        await this.SendEmail(email);
+    }
 
-        /// <summary>
-        /// Sends an email notifying that a chord sheet has been edited or created and is awaiting approval.
-        /// </summary>
-        /// <param name="submission"></param>
-        /// <param name="original">The original chord sheet being edited. If this is a new chord sheet being submitted, this will be null.</param>
-        /// <param name="token">The approval token.</param>
-        /// <returns></returns>
-        public async Task SendChordSubmissionEmail(ChordSubmission submission, ChordSheet? original, string token)
-        {
-            var htmlBody = original == null ?
-                this.GetNewChordsEmailHtml(submission, token) :
-                this.GetUpdatedChordsEmailHtml(submission, token);
-            var email = this.CreateEmail("MessianicChords: chord chart edit awaiting your approval", new EmailAddress(settings.UploadedAttachmentEmailRecipient), null, htmlBody);
-            await this.SendEmail(email);
-        }
+    private string GetNewChordsEmailHtml(ChordSubmission submission, string token)
+    {
+        return "" +
+            "<h1>New chords uploaded to MessianicChords</h1>" +
+            "<p>" +
+            $"<a href='https://messianicchords.com/chordsubmissions/review?id={submission.Id}&token={token}'>Review new chord chart</a>" +
+            "</p>";
+    }
 
-        private string GetNewChordsEmailHtml(ChordSubmission submission, string token)
-        {
-            return "" +
-                "<h1>New chords uploaded to MessianicChords</h1>" +
-                "<p>" +
-                $"<a href='https://messianicchords.com/chordsubmissions/review?id={submission.Id}&token={token}'>Review new chord chart</a>" +
-                "</p>";
-        }
+    private string GetUpdatedChordsEmailHtml(ChordSubmission submission, string token)
+    {
+        return "" +
+            "<h1>Chord chart edited on MessianicChords</h1>" +
+            "<p>" +
+            $"<a href='https://messianicchords.com/chordsubmissions/review?id={submission.Id}&token={token}'>Review edited chord chart</a>" +
+            "</p>";
+    }
 
-        private string GetUpdatedChordsEmailHtml(ChordSubmission submission, string token)
+    private SendGridMessage CreateEmail(string subject, EmailAddress to, string? plainText, string html)
+    {
+        var email = new SendGridMessage
         {
-            return "" +
-                "<h1>Chord chart edited on MessianicChords</h1>" +
-                "<p>" +
-                $"<a href='https://messianicchords.com/chordsubmissions/review?id={submission.Id}&token={token}'>Review edited chord chart</a>" +
-                "</p>";
-        }
+            Subject = subject,
+            From = new EmailAddress(settings.EmailSender, "Messianic Chords"),
+            PlainTextContent = plainText,
+            HtmlContent = html
+        };
+        email.AddTo(to);
 
-        private SendGridMessage CreateEmail(string subject, EmailAddress to, string? plainText, string html)
+        return email;
+    }
+
+    private async Task SendEmail(SendGridMessage email)
+    {
+        // Store the email in the database.
+        var dbEmail = Email.FromSmtpMessage(email);
+
+        var client = new SendGrid.SendGridClient(settings.SendGridKey);
+        try
         {
-            var email = new SendGridMessage
+            var result = await client.SendEmailAsync(email);
+            if (result.IsSuccessStatusCode)
             {
-                Subject = subject,
-                From = new EmailAddress(settings.EmailSender, "Messianic Chords"),
-                PlainTextContent = plainText,
-                HtmlContent = html
-            };
-            email.AddTo(to);
-            return email;
-        }
-
-        private async Task SendEmail(SendGridMessage email)
-        {
-            var client = new SendGrid.SendGridClient(settings.SendGridKey);
-            try
-            {
-                var result = await client.SendEmailAsync(email);
-                if (result.IsSuccessStatusCode)
-                {
-                    logger.LogInformation("Email sent successfully, status code {statusCode}", result.StatusCode);
-                }
-                else
-                {
-                    logger.LogError("Email failed to send. Status code {code} was non-successful.", result.StatusCode);
-                }
+                logger.LogInformation("Email sent successfully, status code {statusCode}", result.StatusCode);
+                dbEmail.Sent = DateTimeOffset.UtcNow;
             }
-            catch (Exception error)
+            else
             {
-                logger.LogError(error, "Email sending failed due to exception.", error);
-                throw;
+                logger.LogError("Email failed to send. Status code {code} was non-successful.", result.StatusCode);
+                dbEmail.SendingErrorMessage = $"Email failed to send. Status code {result.StatusCode} was non-successful.";
             }
+        }
+        catch (Exception error)
+        {
+            logger.LogError(error, "Email sending failed due to exception.");
+            dbEmail.SendingErrorMessage = $"Email failed to send due to exception: {error}";
+            throw;
+        }
+        finally
+        {
+            await TrySaveDbEmail(dbEmail);
+        }
+    }
+
+    private async Task TrySaveDbEmail(Email dbEmail)
+    {
+        try
+        {
+            var dbSession = db.OpenAsyncSession();
+            await dbSession.StoreAsync(dbEmail);
+            dbSession.SetRavenExpiration(dbEmail, DateTime.UtcNow.AddDays(120));
+            await dbSession.SaveChangesAsync();
+        }
+        catch (Exception dbException)
+        {
+            logger.LogError(dbException, "Unable to save email to database due to error.");
         }
     }
 }
