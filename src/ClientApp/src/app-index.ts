d08beb4cfda89aa99e47c782f9a17118ad270a29
Judah Gabriel Himango <judahgabriel@gmail.com>
2026-06-10 09:55:22 -0700

    Trying without footer

--- src/ClientApp/src/app-index.ts
+++ src/ClientApp/src/app-index.ts
@@ -37,7 +37,7 @@ export class AppIndex extends LitElement {
                     ${this.route}
                 </main>
 
-                <app-footer></app-footer>
+                
             </div>
         `;
     }
